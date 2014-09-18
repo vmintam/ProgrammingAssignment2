52ef502f8eaf0002086cab0acc6aa5e222f9c20c
vmintam <vmintam@gmail.com>
2014-09-18 15:33:49 +0700

    comment some function

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,20 @@
 ## return a list contain functions: get, set, getMatrix, setMatrix
 
 makeCacheMatrix <- function(x = matrix()){
+        #initial s to NULL
         s <- NULL
+        # set function assign x to new enviroment :y and S to NULL
         set <- function(y){
                 x <<- y
                 s <<- NULL
         }
+        # get x - matrix
         get <- function() x
+        # caculate invert matrix
         setMatrix <- function(solve) s <<- solve
+        # get invert matrix
         getMatrix <- function() s
+        #return list
         list(set = set, get = get, 
              setMatrix = setMatrix,
              getMatrix = getMatrix
@@ -23,14 +29,17 @@ makeCacheMatrix <- function(x = matrix()){
 ## perform solve(matrix)
 
 cacheSolve <- function(x, ...){
-        ## Return a matrix that is the inverse of 'x'
+        ## get matrix(x) inverted.
         s <- x$getMatrix()
+        # if not null, s cached
         if(!is.null(s)){
                 message("getting cached matrix")
                 return(s)
         }
+        # if not cached, caculate invert matrix
         matrix <- x$get()
         s <- solve(matrix, ...)
         x$setMatrix(s)
+        # return invert matrix
         s
 }
